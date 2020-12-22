0514071c83eb3f130d05d3a5c7efe55dd523e03d
bthnadgzl <batuhanadigzl@gmail.com>
2020-12-22 15:22:11 +0300

    Initial commit

---
+++ homework1.py
@@ -0,0 +1,10 @@
+
+us1=float(input("enter value"))
+us2=float(input("enter velue"))
+us3=float(input("enter value"))
+us4=float(input("enter value"))
+us5=float(input("enter value"))
+
+
+
+print("1. value = {} type={}\n2. value = {} type={}\n3.value = {} type{}\n4. value= {} type={}\n5. value= {} type={}".format(us1,type(us1),us2,type(us2),us3,type(us3),us4,type(us4),us5,type(us5)))
